9fd8a24059d7992f229b6a5514e55189624dbd6a
Max Levtov <maxlevtov@gmail.com>
2021-08-05 16:33:40 +0300

    init

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 FROM node:14
 COPY package*.json ./img
 RUN npm install
-COPY . .
 ENV GENERATE_SOURCEMAP=false
 EXPOSE 3000
+COPY . .
 CMD ["npm", "start","--max-old-space-size=8192"]
--- server/index.js
+++ server/index.js
@@ -6,13 +6,16 @@ const addItem = require('./routes/addItem');
 const updateItem = require('./routes/updateItem');
 const deleteItem = require('./routes/deleteItem');
 const cors = require('cors');
-
+// import * as events from './mocks/events.json';
+const events = require('./mocks/events');
 app.use(cors());
 app.use(require('body-parser').json());
 app.use(express.static(__dirname + '/static'));
 
-app.get('/',(req,res)=>{
-        res.send('INDEX.JS RUNING !');
+app.get('/', (req, res) => {
+    console.log('INDEX.JS RUNING !');
+    res.json(events);
+
 });
 
 app.get('/items', getItems);
@@ -20,21 +23,22 @@ app.post('/items', addItem);
 app.put('/items/:id', updateItem);
 app.delete('/items/:id', deleteItem);
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+// app.listen(3000, () => console.log('Listening on port 3000'));
+console.log(process.env.PORT);
+db.init().then(() => {
+    app.listen(3000, () =>
+        console.log('Listening on port http://localhost:3000'));
+}).catch((err) => {
+    console.error(err);
+    process.exit(1);
+});
 
-// db.init().then(() => {
-//     app.listen(3000, () => console.log('Listening on port 3000'));
-// }).catch((err) => {
-//     console.error(err);
-//     process.exit(1);
-// });
+const gracefulShutdown = () => {
+    db.teardown()
+        .catch(() => { })
+        .then(() => process.exit());
+};
 
-// const gracefulShutdown = () => {
-//     db.teardown()
-//         .catch(() => { })
-//         .then(() => process.exit());
-// };
-
-// process.on('SIGINT', gracefulShutdown);
-// process.on('SIGTERM', gracefulShutdown);
-// process.on('SIGUSR2', gracefulShutdown); // Sent by nodemon
+process.on('SIGINT', gracefulShutdown);
+process.on('SIGTERM', gracefulShutdown);
+process.on('SIGUSR2', gracefulShutdown); // Sent by nodemon
